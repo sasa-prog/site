29f68364b200f5d6d2e088b33f00633bfcb0bb30
任意ãsasa-prog <sasagodan@gmail.com>
2022-08-14 12:19:24 +0900

    done

--- libs/client.js
+++ libs/client.js
@@ -2,7 +2,7 @@ import { createClient } from "microcms-js-sdk";
 
 export const client  = createClient({
     serviceDomain: "sasa-blog",
-    apiKey: process.env.API_KEY,
+    apiKey: "716edcc52a6f493dba8e18ee43dcb68523b2",
 });
 
  
